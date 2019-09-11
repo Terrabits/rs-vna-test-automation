d5fe5078d7526770b57f8139115e1d79f01909c2
Nick Lalic <nick.lalic@gmail.com>
2019-09-11 00:19:03 -0700

    🚧 Made MVC available on window(global), with some console.logging.

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,9 @@ import React        from 'react';
 import ReactDOM     from 'react-dom';
 import 'bootstrap';
 import './scss/styles.scss';
+import Model        from './model';
 import App          from './components/App';
+import Controller   from './controller';
 import sidebarItems from './sidebar-items';
 
 // App:
@@ -12,16 +14,31 @@ import sidebarItems from './sidebar-items';
 // - onBack=                     (index)=>{}
 // - onNext=                     (index)=>{}
 
+const controller = new Controller(null, null);
 const backClicked = (event=null) => {
   if (event) {
     event.preventDefault();
   }
-  console.log('back clicked');
+  controller.back();
 };
 const nextClicked = (event=null) => {
   if (event) {
     event.preventDefault();
   }
-  console.log('next clicked');
+  controller.next();
 };
-ReactDOM.render(<App sidebarItems={sidebarItems} onBackClicked={backClicked} onNextClicked={nextClicked}/>, document.getElementById('root'));
+window.view = ReactDOM.render(<App sidebarItems={sidebarItems} onBackClicked={backClicked} onNextClicked={nextClicked}/>, document.getElementById('root'));
+
+window.model      = new Model();
+window.controller = controller;
+
+window.model.socket.open().then(() => {
+  console.log('model is open!');
+  window.model.socket.closed().then((event) => {
+    console.log('model closed!');
+  }).catch((param) => {
+    console.log(`window.model.socket.closed()..catch(param=${param})`);
+  });
+}, () => {
+  console.log('model failed to open!');
+});
