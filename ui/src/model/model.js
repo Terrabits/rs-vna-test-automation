64788ed9c96e4bb6499d5a775128dd062518afb3
Nick Lalic <nick.lalic@gmail.com>
2019-09-11 00:17:44 -0700

    🚧 Initial implementation of Model.

---
+++ ui/src/model/model.js
@@ -0,0 +1,107 @@
+import Socket from './socket';
+
+// type conversions
+function toBool(result) {
+  return result.trim().toLowerCase() === 'true';
+}
+function toJson(result) {
+  return JSON.parse(result);
+}
+function toNumber(result) {
+  return Number(result.trim());
+}
+
+// promises abound.
+class Model {
+  constructor() {
+    this.socket = new Socket();
+  }
+
+  // vna
+  async isVnaConnected() {
+    return await this.socket.query('is_vna_connected?\n', toBool);
+  }
+  connectToVna(address) {
+    this.socket.send(`connect_to_vna ${address}\n`);
+  }
+  disconnectFromVna() {
+    this.socket.send('disconnect_from_vna\n');
+  }
+
+  // project
+  async isProjectOpen() {
+    return await this.socket.query('is_project_open?\n', toBool);
+  }
+  openProject(filename) {
+    this.socket.send(`open_project ${filename}\n`)
+  }
+  async readProject() {
+    return await this.socket.query('read_project?\n', toJson);
+  }
+  closeProject() {
+    this.socket.send('close_project\n');
+  }
+
+  // calibration
+  async calGroups() {
+    const response = await this.query('cal_groups?\n');
+    return response.split(',');
+  }
+  async calUnits() {
+    return await this.socket.query('cal_units?\n', toNumber);
+  }
+  async calibrationSteps() {
+    return await this.socket.query('calibration_steps?\n', toNumber);
+  }
+  async calibrationStep(i) {
+    if (i < 0) {
+      throw new Error('Calibration step must be >0');
+    }
+    const steps = await this.calibrationSteps();
+    if (i >= await this.calibrationSteps()) {
+      throw new Error(`Calibration step must be <${steps}`);
+    }
+    return await this.socket.query(`calibration_step? ${i}\n`, toJson);
+  }
+  startCalibration() {
+    this.socket.send('start_calibration\n');
+  }
+  async performCalibrationStep(step) {
+    const SCPI      = `perform_calibration_step? ${step}\n`;
+    return await this.socket.query(SCPI, toBool);
+  }
+  async applyCalibration(calGroup=null) {
+    calGroup        = calGroup || '""';
+    return await this.socket.send(`apply_calibration? ${calGroup}\n`, toBool);
+  }
+
+  // measurement
+  async measurementSteps() {
+    return await this.socket.query('measurement_steps?\n', toNumber);
+  }
+  async measurementStep(i) {
+    if (i < 0) {
+      throw new Error('Measurement step must be >0');
+    }
+    const steps = await this.measurementSteps();
+    if (i >= await this.measurementSteps()) {
+      throw new Error(`Measurement step must be <${steps}`);
+    }
+    return await this.socket.query(`measurement_step? ${i}\n`, toJson);
+  }
+  startMeasurementsFor(serialNo, calGroup=null) {
+    calGroup = calGroup || '""';
+    const scpi = `start_measurements_for ${serialNo} ${calGroup}\n`;
+    this.socket.send(scpi);
+  }
+  async performMeasurementStep(index) {
+    const scpi = `perform_measurement_step? ${index}\n`
+    return await this.socket.query(scpi, toBool);
+  }
+  saveMeasurements(path=null) {
+    path = path || '.';
+    this.socket.send(`save_measurements ${path}\n`);
+  }
+};
+
+export default Model;
---
+++ ui/src/model/socket.js
@@ -0,0 +1,81 @@
+const HOSTNAME      = window.location.hostname
+const PORT          = 60652;
+function url(hostname, port) {
+  return `ws://${hostname}:${port}`
+}
+const URL           = url(HOSTNAME, PORT);
+
+class Socket {
+  constructor(url=null) {
+    this.close();
+    if (url) {
+      this.open(url);
+    }
+  }
+  isOpen() {
+    if (!this.socket) {
+      return false;
+    }
+    return this.socket.readyState === WebSocket.OPEN;
+  }
+  open(url=URL) {
+    this.socket = new WebSocket(url);
+    return new Promise((resolve, reject) => {
+      const onopen = () => {
+        resolve();
+      };
+      const onerror = (event) => {
+        console.log(`Model.open().onerror(event: ${event})`);
+        reject();
+      };
+      this.socket.onopen  = onopen;
+      this.socket.onerror = onerror;
+    });
+  }
+  close() {
+    if (this.isOpen()) {
+      this.socket.close();
+    }
+    this.socket = null;
+  }
+  closed() {
+    return new Promise((resolve, reject) => {
+      if (!this.isOpen()) {
+        reject();
+      }
+      this.socket.onclose = (event) => {
+        resolve(event);
+      }
+    });
+  }
+  send(message) {
+    if (!this.isOpen()) {
+      throw new Error('NOT OPEN!');
+    }
+    this.socket.send(message);
+  }
+  read(typefn=undefined) {
+    typefn = typefn || ((i) => { return i; });
+    return new Promise((resolve, reject) => {
+      this.socket.onmessage = (event) => {
+        const blob     = event.data;
+        const reader   = new FileReader();
+        reader.onload  = () => {
+          const result = typefn(reader.result);
+          resolve(result);
+        };
+        reader.onerror = () => {
+          reject();
+        };
+        reader.readAsText(blob);
+      };
+    });
+  }
+  query(message, typefn=undefined) {
+    const promise = this.read(typefn);
+    this.socket.send(message);
+    return promise;
+  }
+}
+
+export default Socket;
