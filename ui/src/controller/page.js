c715c5dccca772ccd17babcf5c629c8a638ce6e2
Nick Lalic <nick.lalic@gmail.com>
2019-09-11 22:05:37 -0700

    🚧 Still fleshing out the index/page mechanism...

--- ui/src/controller/page.js
+++ ui/src/controller/page.js
@@ -3,35 +3,11 @@ class Page {
     this.name = name;
     this.step = step
   }
-
   isCalibrating() {
     return this.name === "PerformCalibrationPage";
   }
   isMeasuring() {
     return this.name === "MeasurePage";
-  }
-
-  back() {
-    //
-    return this;
-  }
-  next() {
-    //
-    return this;
-  }
-
-  cancel() {
-    if (this.isCalibrating()) {
-      // cancel
-    }
-    if (this.isMeasuring()) {
-      // cancel
-    }
-    // else do nothing
-  }
-  reset() {
-    this.name = 'SetupPage';
-    this.step = -1;
   }
 }
 
