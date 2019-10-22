577a92f898af7eac5659c57d4c964cd881958c1e
Nick Lalic <nick.lalic@gmail.com>
2019-10-21 21:14:26 -0700

    🚧 Handle life cycle of server process in main.

--- desktop/main.js
+++ desktop/main.js
@@ -70,6 +70,10 @@ app.on('window-all-closed', () => {
   }
 });
 
+app.on('will-quit', () => {
+  serverProcess.kill();
+});
+
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
